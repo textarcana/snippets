ff207d1ce3335f88fd048c86d9ba707b52a0d220
textarcana <noah@onemorebug.com>
2011-03-30 21:25:16 -0400

    how to detach all sessions from a GNU screen instance cribbed from http://aperiodic.net/screen/quick_reference

---
+++ bash/screen_detach_all_other_sessions.sh
@@ -0,0 +1,6 @@
+screen -dRR
+
+# Run from within a screen session, this will bounce you out AND
+# disconnect any other sessions attached to that screen instance.  
+#
+# See http://aperiodic.net/screen/quick_reference
