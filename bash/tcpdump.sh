8bad8a126494d45995c98b7c6914fa033e2869ea
textarcana <noah@onemorebug.com>
2011-04-30 19:37:26 -0400

    extreme packet pr0n

--- bash/tcpdump.sh
+++ bash/tcpdump.sh
@@ -22,6 +22,15 @@ sudo tcpdump -S -s 0 port 80 and net 10.0.1.0/24
 
 sudo tcpdump -vvvSA -s 0 -i en1 port 80
 
+# just show what's in the packets 
+# (bytes on the left, ASCII on the right)
+
+sudo tcpdump -qXX -s 0 port 80
+
+# extreme packet pr0n
+
+sudo tcpdump -vvvSeXX -s 0 port 80
+
 # Show all hosts on the local subnet by
 # performing reverse DNS lookups.
 # Nameservers listed in resolv.conf
