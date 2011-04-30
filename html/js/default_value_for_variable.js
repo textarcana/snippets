a5e7af6e37a00d48890b9344c7866153c830e178
textarcana <noah@onemorebug.com>
2011-04-29 21:44:26 -0400

    how to set a JavaScript variable, only if it has not already been assigned a value

---
+++ html/js/default_value_for_variable.js
@@ -0,0 +1,10 @@
+/* 
+   To set foo to a custom value, set 
+       var foo = 123;
+   any time before this script is loaded.
+
+   If you don't set foo, the default value will be used.
+  */
+
+var default_for_foo = 789;
+var refresh_timeout = (typeof foo !== 'undefined' ? foo : default_for_foo);
