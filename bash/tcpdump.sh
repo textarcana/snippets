43d27689443c59479ce14082749f9fdc6962e2a1
textarcana <noah@onemorebug.com>
2011-04-30 18:33:13 -0400

    fun with packets

--- bash/tcpdump.sh
+++ bash/tcpdump.sh
@@ -1,18 +1,18 @@
 # listen for traffic to / from example.com
 
-sudo tcpdump -i eth0 host example.com
+sudo tcpdump -S -i eth0 host example.com
 
 # show all udp, icmp or tcp packet traffic
 
-sudo tcpdump udp
+sudo tcpdump -S udp
+
+# show ethernet traffic / MAC addresses
+
+sudo tcpdump -qeS -s 0 -i en1 port 8080
 
 # im in ur web browzar, sniffin ur pakkitz
 
-sudo tcpdump -s 1514 -c 1024 port 80
-
-# Packet porn
-
-sudo tcpdump -vvSX -s 1514 port 80
+sudo tcpdump -vvvnSA -s 0 -i en1 port 80
 
 # Show all hosts on the local subnet by
 # performing reverse DNS lookups.
@@ -22,9 +22,13 @@ nmap -sL 10.0.1.0/24
 
 # Find hosts that respond to pings.
 
-nmap -sP 10.0.1.0/24 
+nmap -sP -T4 --reason 10.0.1.0/24 
 
-# look for an open port in the 8k range
+# show all packets sent and received by nmap
+
+nmap -sP -T4 --reason --packet-trace 10.0.1.0/28
+
+# Look for an open port in the 8k range
 
 nc -vz 10.0.1.9 8000-8999 2> /dev/null
 
