a233ceb25a3c83a248fb885c915b4518343b3b8d
textarcana <noah@onemorebug.com>
2011-04-30 16:56:34 -0400

    resolv.conf is where the nameservers are at

--- bash/tcpdump.sh
+++ bash/tcpdump.sh
@@ -12,10 +12,11 @@ sudo tcpdump -s 1514 -c 1024 port 80
 
 # Packet porn
 
-sudo tcpdump -SX -s 1514 port 80
+sudo tcpdump -vvSX -s 1514 port 80
 
-# Show all hosts on a network by
+# Show all hosts on the local subnet by
 # performing reverse DNS lookups.
+# Nameservers listed in resolv.conf
 
 nmap -sL 10.0.1.0/24
 
@@ -26,3 +27,4 @@ nmap -sP 10.0.1.0/24
 # look for an open port in the 8k range
 
 nc -vz 10.0.1.9 8000-8999 2> /dev/null
+
