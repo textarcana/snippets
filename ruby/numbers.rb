8dff88b9c3ccb23266b97e3a66479a7af5fae0ce
textarcana <noah@onemorebug.com>
2011-06-11 18:11:02 -0400

    how to use the humanize gem to convert an integer into English, and how to compute factorials

--- ruby/numbers.rb
+++ ruby/numbers.rb
@@ -1,2 +1,9 @@
+# humanize
+require 'humanize'
+puts 1427228946471605830963606579751332537625641011.humanize
+
+# factorials
+puts "factorial 5: " + 5.downto(1).inject(:*)
+
 # the golden ratio
 PHI = (1 + Math.sqrt(5)) / 2
