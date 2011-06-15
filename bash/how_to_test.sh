3a5ab72bfbd917a43c352ed5808f4ef86162ee76
textarcana <noah@onemorebug.com>
2011-06-15 14:41:03 -0400

    cannot use echo to produce strings that are to be passed to sha1sum
    as yet I have no idea why this is.  Using Perl to print the strings works just fine.

--- bash/how_to_test.sh
+++ bash/how_to_test.sh
@@ -8,5 +8,10 @@ ps ax | grep selenium | cut -d' ' -f2 | xargs kill
 while ! nc -vz localhost 4444; do sleep 1; done
 
 # validate that two sha1 hashes are identical
+#
+# NOTE: you really do need to use perl -e 'print "foo"' here, rather
+# than echo "foo" (at least this is the case on Mac OS X).  I am not
+# sure why this is the case :-\ but using echo will result in your not
+# getting the expected hash value.
 
-test `echo doh! | shasum | cut -d' ' -f1` == `echo doh! | shasum | cut -d' ' -f1` && echo ok || echo nope
+test `perl -e "print qq{foo}" | shasum | cut -d' ' -f1` == `perl -e "print qq{foo}" | shasum | cut -d' ' -f1` && echo hashes are the same || echo OOPS! hash mismatch
