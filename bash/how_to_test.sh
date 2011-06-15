948612a2bd930e369c8f4bee9b38977f094483a7
textarcana <noah@onemorebug.com>
2011-06-15 14:48:30 -0400

    oh right, echo adds a newline to the end of the string by default
    DERP

--- bash/how_to_test.sh
+++ bash/how_to_test.sh
@@ -9,9 +9,8 @@ while ! nc -vz localhost 4444; do sleep 1; done
 
 # validate that two sha1 hashes are identical
 #
-# NOTE: you really do need to use perl -e 'print "foo"' here, rather
-# than echo "foo" (at least this is the case on Mac OS X).  I am not
-# sure why this is the case :-\ but using echo will result in your not
-# getting the expected hash value.
+# NOTE: When using echo, you have to include the -n option, otherwise
+# it will glom a newline onto the end of the string and your hash
+# won't be what you expect.
 
-test `perl -e "print qq{foo}" | shasum | cut -d' ' -f1` == `perl -e "print qq{foo}" | shasum | cut -d' ' -f1` && echo hashes are the same || echo OOPS! hash mismatch
+test `echo -n foo | shasum | cut -d' ' -f1` == `perl -e "print qq{foo}" | shasum | cut -d' ' -f1` && echo hashes are the same || echo OOPS! hash mismatch
