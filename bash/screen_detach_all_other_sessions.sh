3167fda912209efa96691decb8aa3a54246fc734
textarcana <noah@onemorebug.com>
2011-07-19 11:21:51 -0400

    how to make screen redraw all windows: unplug it and plug it back in

--- bash/screen_detach_all_other_sessions.sh
+++ bash/screen_detach_all_other_sessions.sh
@@ -3,4 +3,6 @@ screen -dRR
 # Run from within a screen session, this will bounce you out AND
 # disconnect any other sessions attached to that screen instance.  
 #
+# This will cause screen to redraw all its windows when you reconnect.
+#
 # See http://aperiodic.net/screen/quick_reference
