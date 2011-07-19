e25a9d42725054f2f6e1b4110f02fae5f3177a68
Noah Sussman <nsussman@etsy.com>
2011-07-19 01:35:21 -0400

    keep constants together

--- ruby/numbers.rb
+++ ruby/numbers.rb
@@ -5,15 +5,14 @@ approximate_age_of_the_universe_in_English = (433 * 10**15).humanize
 
 seconds_since_epoch_in_English = `date '+%s'`.to_i.humanize
 
+# the golden ratio
+PHI = (1 + Math.sqrt(5)) / 2
+
 # factorials
 
 def factorial n
   (1..n).inject{|base,x| base * x }
 end
-
-
-# the golden ratio
-PHI = (1 + Math.sqrt(5)) / 2
 
 # triangular numbers
 #
