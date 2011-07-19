18ed41bafcf6efc82652aaf7ca9d10c8b8a0fafa
Noah Sussman <nsussman@etsy.com>
2011-07-19 01:34:01 -0400

    triangular and factorial functions for Ruby 1.8.6
        * cuz centOS has the Ruby 1.8.6

--- ruby/numbers.rb
+++ ruby/numbers.rb
@@ -6,7 +6,22 @@ approximate_age_of_the_universe_in_English = (433 * 10**15).humanize
 seconds_since_epoch_in_English = `date '+%s'`.to_i.humanize
 
 # factorials
-puts "factorial 5: " + 5.downto(1).inject(:*)
+
+def factorial n
+  (1..n).inject{|base,x| base * x }
+end
+
 
 # the golden ratio
 PHI = (1 + Math.sqrt(5)) / 2
+
+# triangular numbers
+#
+# This function can be used to compute complexity of communication.
+# Think of it as the number of possible bi-directional connections in
+# a network.  Or, more to the point, the number of ways a message can
+# be passed between n people, see Brooks' Law
+
+def triangular number
+  (0..number).inject{|sum,x| sum + x }
+end
