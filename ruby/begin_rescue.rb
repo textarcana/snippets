1ae330c4524fd7533741c9e78b4f20b7cdd0fa0d
textarcana <noah@onemorebug.com>
2011-08-03 15:23:44 -0400

    fun with rescue, raise and ensure

--- ruby/begin_rescue.rb
+++ ruby/begin_rescue.rb
@@ -1,17 +1,24 @@
-def foo
-
-  begin
-    nil.bad_method
-  rescue NoMethodError => e
-    puts %{EXCEPTION ENCOUNTERED.  REMAIN CALM.}
-    puts %{Exception Message:    #{e.message}}
-    puts %{Exception Backtrace:\n    #{e.backtrace.join("\n    ")}}
+module BadThings
+  def do_something_bad
+    begin
+      nil.bad_method
+    rescue NoMethodError => e
+      puts %{EXCEPTION ENCOUNTERED.  REMAIN CALM.}
+      puts %{Exception Message:    #{e.message}}
+      puts %{Exception Backtrace:\n    #{e.backtrace.join("\n    ")}}
+    ensure
+      if e
+        puts %{\nRaising Exception, Stand By...}
+        raise e
+      end
+    end
   end
 
+  def throw_an_exception
+    do_something_bad
+  end
 end
 
-def bar
-  foo
-end
+include BadThings
 
-bar
+BadThings::throw_an_exception
