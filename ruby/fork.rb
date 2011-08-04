b58d4d213c73d66cd40a56601fbf6f6c3a679e7b
Noah Sussman <nsussman@etsy.com>
2011-08-04 10:01:48 -0400

    more fun with fork

--- ruby/fork.rb
+++ ruby/fork.rb
@@ -1,10 +1,13 @@
 # fork off a child process whose execution does not block the further
 # execution of this script
 
+foo = 1
+
 pid = fork do
-  sleep 10
+  puts "foo is #{foo}"
+  sleep 2
   File.open("hello_world.txt", 'a') {|f| f.puts('hi world')}
-  puts '...then a file gets printed 10 seconds later'
+  puts '...then a file gets printed 2 seconds later'
 end
 Process.detach(pid)
 
