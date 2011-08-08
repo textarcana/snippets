5245b98ca2c34c8b8163d6a86228cbfb29f2930e
textarcana <noah@onemorebug.com>
2011-08-08 17:10:26 -0400

    force unix-ish filenames, not windows any more

--- bash/wget_scraper.sh
+++ bash/wget_scraper.sh
@@ -15,7 +15,7 @@ test -e $NAME_FOR_LOG && mv $NAME_FOR_LOG.log $NAME_FOR_LOG.`date +%C%y-%m-%d-at
 wget -r -l inf -H -nc -w$WAIT_TIME \
     --random-wait --limit-rate=80k -erobots=off \
     --page-requisites -D $DOMAIN \
-    --restrict-file-names=windows \
+    --restrict-file-names=unix \
     -U "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.14) Gecko/2009090214" \
     -o $NAME_FOR_LOG.log \
     http://$DOMAIN/
