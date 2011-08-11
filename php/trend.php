3247b0e07aaed9da785cb6f5d30ba70ea4d4a701
textarcana <noah@onemorebug.com>
2011-08-11 00:00:40 -0400

    typo

--- php/trend.php
+++ php/trend.php
@@ -1,7 +1,7 @@
 |<?php
-/**
- * Trend function is from http://phpsnips.com/snippet.php?id=45 
- */
+   /**
+    * Trend function is from http://phpsnips.com/snippet.php?id=45 
+    */
 
 function trend($array, $output) { 
   if (!is_array($array)) { 
@@ -9,37 +9,37 @@ function trend($array, $output) {
   }
   switch($output) { 
   case 'mean': 
-      $count = count($array); 
-      $sum = array_sum($array); 
-      $total = $sum / $count; 
-      break; 
+    $count = count($array); 
+    $sum = array_sum($array); 
+    $total = $sum / $count; 
+  break; 
   case 'median': 
     rsort($array); 
     $middle = round(count($array) / 2); 
     $total = $array[$middle-1]; 
-    break; 
+  break; 
   case 'mode': 
     $v = array_count_values($array); 
     arsort($v); 
     foreach($v as $k => $v) {$total = $k; break; } 
-    break; 
+  break; 
   case 'range': 
     sort($array); 
     $sml = $array[0]; 
     rsort($array); 
     $lrg = $array[0]; 
     $total = $lrg - $sml; 
-    break; 
-    return $total; 
-
-  } 
-
-  function trends_for($list) {
-    return sprintf(
-                   "%s\nmean: %f\nmedian: %f\nmode: %f\nrange: %f", 
-                   implode(' ,', $list), 
-                   trend($list, 'mean'), 
-                   trend($list, 'median'), 
-                   trend($list, 'mode'), 
-                   trend($list, 'range'));
+  break; 
   }
+  return $total; 
+}
+
+function trends_for($list) {
+  return sprintf(
+                 "%s\nmean: %f\nmedian: %f\nmode: %f\nrange: %f", 
+                 implode(' ,', $list), 
+                 trend($list, 'mean'), 
+                 trend($list, 'median'), 
+                 trend($list, 'mode'), 
+                 trend($list, 'range'));
+}
