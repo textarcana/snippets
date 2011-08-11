0c0fd085e2bc9fa132a861ed50acf1872969bed9
textarcana <noah@onemorebug.com>
2011-08-11 00:32:58 -0400

    collect numbers into log10 buckets

--- php/trend.php
+++ php/trend.php
@@ -39,19 +39,47 @@ function trend($list, $type) {
   case 'high':
     $total = max($list);
   break;
+  case 'log10_buckets':
+    $values=array(
+                  '0 to 10'         => array(), 
+                  '11 to 100'       => array(),
+                  '101 to 1000'     => array(),
+                  '1001 or greater' => array(),
+                  );
+    rsort($list);
+    foreach($list as $number){
+      if ($number <= 10){
+        array_push($values['0 to 10'], $number);
+      } else if ($number <= 100) {
+        array_push($values['11 to 100'], $number);
+      } else if ($number <= 1000) {
+        array_push($values['101 to 1000'], $number);
+      } else {
+        array_push($values['1001 or greater'], $number);
+      }
+      $total = sprintf(
+                       "<10: %s\n11-100: %s\n101-1000: %s\n>1000: %s",
+                       implode(', ', $values['0 to 10']),
+                       implode(', ', $values['11 to 100']),
+                       implode(', ', $values['101 to 1000']), 
+                       implode(', ', $values['1001 or greater'])
+                       );
+    }
+  break;
  }
   return $total; 
 }
 
 function trends_for($list) {
   return sprintf(
-                 "%.2s\nmean: %.2f\nmedian: %.2f\nmode: %.2f\nrange: %.2f\nlow: %.2f\nhigh: %.2f", 
+                 "%.2s\nmean: %.2f\nmedian: %.2f\nmode: %.2f\nrange: %.2f\nlow: %.2f\nhigh: %.2f\n%s", 
                  implode(' ,', $list), 
                  trend($list, 'mean'), 
                  trend($list, 'median'), 
                  trend($list, 'mode'), 
                  trend($list, 'range'),
                  trend($list, 'low'),
-                 trend($list, 'high')
+                 trend($list, 'high'),
+                 trend($list, 'log10_buckets')
                  );
 }
