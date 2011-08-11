b63e8b4d30b5e6941d2c7083b22dc3c4b5561fab
textarcana <noah@onemorebug.com>
2011-08-11 01:11:06 -0400

    copy of the rasterize.js (working) example code for PhantomJS, a headless Webkit implementation that is capable of taking screenshots!
    see http://code.google.com/p/phantomjs/wiki/QuickStart#Rendering

---
+++ javascript/screenshot.js
@@ -0,0 +1,26 @@
+/**
+ * take a screenshot with PhantomJS
+ * see http://code.google.com/p/phantomjs/wiki/QuickStart#Rendering
+ */
+
+var page = new WebPage(),
+    address, output, size;
+
+if (phantom.args.length < 2 || phantom.args.length > 3) {
+    console.log('Usage: rasterize.js URL filename');
+    phantom.exit();
+} else {
+    address = phantom.args[0];
+    output = phantom.args[1];
+    page.viewportSize = { width: 600, height: 600 };
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('Unable to load the address!');
+        } else {
+            window.setTimeout(function () {
+                page.render(output);
+                phantom.exit();
+            }, 200);
+        }
+    });
+}
