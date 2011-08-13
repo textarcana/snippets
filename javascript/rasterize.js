aef5cbf5081cfc1c8ff3d3bbefa4896e3e90be98
Noah Sussman <nsussman@etsy.com>
2011-08-12 23:10:35 -0400

    try larger viewport size

--- javascript/rasterize.js
+++ javascript/rasterize.js
@@ -14,7 +14,7 @@ if (phantom.args.length < 2 || phantom.args.length > 3) {
 } else {
     address = phantom.args[0];
     output = phantom.args[1];
-    page.viewportSize = { width: 600, height: 600 };
+    page.viewportSize = { width: 1024, height: 768 };
     page.open(address, function (status) {
         if (status !== 'success') {
             console.log('Unable to load the address!');
