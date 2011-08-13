c5aff187bfb0f8dd241f0899eaa850a3481b5d31
Noah Sussman <nsussman@etsy.com>
2011-08-12 22:24:45 -0400

    comment

---
+++ javascript/rasterize.js
@@ -0,0 +1,28 @@
+/**
+ * take a screenshot with PhantomJS
+ *
+ * This is a copy of code from the PhantomJS examples pae,
+ * see http://code.google.com/p/phantomjs/wiki/QuickStart#Rendering
+ */
+
+var page = new WebPage(),
+    address, output, size;
+
+if (phantom.args.length < 2 || phantom.args.length > 3) {
+    console.log('Usage: rasterize.js URL filename');
+    phantom.exit();
+} else {
+    address = phantom.args[0];
+    output = phantom.args[1];
+    page.viewportSize = { width: 600, height: 600 };
+    page.open(address, function (status) {
+        if (status !== 'success') {
+            console.log('Unable to load the address!');
+        } else {
+            window.setTimeout(function () {
+                page.render(output);
+                phantom.exit();
+            }, 200);
+        }
+    });
+}
