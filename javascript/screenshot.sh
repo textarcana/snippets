e91dec7c2b73d91c30b8258b7fbbded2a8e4591b
Noah Sussman <nsussman@etsy.com>
2011-08-12 23:02:01 -0400

    screenshot shell script using PhantomJS and the rasterize.js example code

---
+++ javascript/screenshot.sh
@@ -0,0 +1,7 @@
+export FILE_NAME=`echo $1 | tr -s ':/' '-' | perl -pe 's/http-//'`
+export DATE_STRING=`date +%m-%d-%Y-%H-%M-%S-%Z`.png
+export OUTFILE=$FILE_NAME"_"$DATE_STRING
+
+/usr/bin/phantomjs rasterize.js $1 $OUTFILE
+
+echo $OUTFILE
