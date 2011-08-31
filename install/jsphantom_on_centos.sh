6e40ea3ec72dc77ed7ab455a77153fd643e6221c
Noah Sussman <nsussman@etsy.com>
2011-08-31 00:18:07 -0400

    full install script for phantomjs on centos

--- install/jsphantom_on_centos.sh
+++ install/jsphantom_on_centos.sh
@@ -1,4 +1,20 @@
-# http://stackoverflow.com/questions/5226919/pointing-to-a-different-yum-repository/5257824#5257824
+# First, check your /etc/yum.repos.d/Centos-Base.repo
+#
+# It should look similar to the one included here.  In particular,
+# make sure it has the ATRPMS block, as described in the comments at
+# http://code.google.com/p/phantomjs/wiki/BuildInstructions
 
-wget http://dl.atrpms.net/all/qtwebkit-devel-2.0-3.el6.x86_64.rpm
-sudo yum --nogpgcheck localinstall qtwebkit-devel-2.0-3.el6.x86_64.rpm 
+sudo yum -y install qt47-devel qt47-webkit qt47-webkit-devel sqlite-devel
+
+# set up GPG keys for yum, see
+# http://www.linuxquestions.org/questions/fedora-35/public-key-not-installed-468615/
+
+rpm --import http://packages.atrpms.net/RPM-GPG-KEY.atrpms
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-5
+
+git clone http://github.com/ariya/phantomjs.git && cd phantomjs
+git checkout 1.2
+qmake-qt47 && make
+
+cp bin/phantomjs /usr/bin/
+
