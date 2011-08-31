793d115b20c9002d5b3cdc64e46a3f934dba5a42
Noah Sussman <nsussman@etsy.com>
2011-08-30 23:57:48 -0400

    install jsphantom on centos
    depending on your CentOS config, you may need to make your /etc/yum.repos.d/Centos-Base.repo file look like the one included here

---
+++ install/jsphantom_on_centos.sh
@@ -0,0 +1,4 @@
+# http://stackoverflow.com/questions/5226919/pointing-to-a-different-yum-repository/5257824#5257824
+
+wget http://dl.atrpms.net/all/qtwebkit-devel-2.0-3.el6.x86_64.rpm
+sudo yum --nogpgcheck localinstall qtwebkit-devel-2.0-3.el6.x86_64.rpm 
