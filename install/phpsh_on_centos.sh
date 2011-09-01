3a4afe22fccb7d1f789f8c9582a882a0bf6a8bb2
Noah Sussman <nsussman@etsy.com>
2011-09-01 12:09:29 -0400

    set up PHPSH on centos
    not really sure that ncurses-static is required

--- install/phpsh_on_centos.sh
+++ install/phpsh_on_centos.sh
@@ -1,3 +1,10 @@
 # https://github.com/facebook/phpsh
 
+sudo yum -y install ncurses-devel ncurses-static
 sudo easy_install pysqlite readline
+
+git clone git://github.com/facebook/phpsh.git
+
+cd phpsh
+python setup.py build
+sudo python setup.py install
