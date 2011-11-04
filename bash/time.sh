fc012acf70b49e8e2a3c73a8269dd815d55deb20
Noah Sussman <noah@onemorebug.com>
2011-11-04 10:05:28 -0400

    how to munge seconds since epoch into something a human can grok

--- bash/time.sh
+++ bash/time.sh
@@ -2,3 +2,14 @@
 # see http://www.noah.org/wiki/Test_file_age
 
 $(($(date +%s) - $(stat -c '%Y' foo.txt)))
+
+
+# convert seconds since epoch to human-readable string
+#
+# Mac
+
+date -r 1320415254 "+%Y-%m-%d %H:%M:%S"
+
+# CentOS
+
+date -d@1320415254
