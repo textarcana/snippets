b2614f56037fb1ff451758f31a1ab1c6498e6aa0
Noah Sussman <nsussman@etsy.com>
2011-11-20 17:13:53 -0500

    git log one-line formats and how to make leader boards from them

--- bash/git_tricks.sh
+++ bash/git_tricks.sh
@@ -1,2 +1,7 @@
 # blow away unversioned artifacts
 git clean -dfx
+
+# list unique Git authors within a date range
+
+git log --format="%an" --since="yesterday" . | sort | uniq
+git log --format="%an" --since="June 20 2010" . | sort | uniq
