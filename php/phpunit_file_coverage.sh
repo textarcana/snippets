fcdcfacab4d3e357163a00b9730295fabd9559ed
Noah Sussman <nsussman@etsy.com>
2011-11-20 18:29:59 -0500

    this seems like a reasonable way to determine which PHP class files have a covering PHPUnit test file

--- php/phpunit_file_coverage.sh
+++ php/phpunit_file_coverage.sh
@@ -13,14 +13,27 @@ diff -U0 /tmp/test_file_identifiers /tmp/php_class_file_identifiers | egrep '^-[
 
 diff -U0 /tmp/test_file_identifiers /tmp/php_class_file_identifiers | egrep '^\+[^\+]' | cut -c2- > /tmp/classes_without_tests
 
+export TOTAL_CLASS_FILES=`cat /tmp/php_class_file_identifiers | wc -l | bc`
+export UNTESTED_CLASSES=`cat /tmp/classes_without_tests | wc -l | bc`
+export CLASSES_WITH_TESTS=`echo $TOTAL_CLASS_FILES - $TESTED_CLASSES | bc`
+export PERCENTAGE_OF_TESTED_CLASSES=`echo "scale=4; $CLASSES_WITH_TESTS / $TOTAL_CLASS_FILES * 100" | bc | cut -c-4`
+
 git log -n1 --format="As of commit %H on %cd"
 
-echo 'Tests without classes\n\n'
+echo "\n\t~$PERCENTAGE_OF_TESTED_CLASSES% file coverage for $PHPLIB"
+
+echo "\n\t$CLASSES_WITH_TESTS PHP class files with tests"
+
+echo "\t$UNTESTED_CLASSES without tests"
+
+echo "\t$TOTAL_CLASS_FILES PHP class files total found in $PHPLIB"
+
+echo '\n\nUntested Classes\n\n'
+
+cat /tmp/classes_without_tests | perl -lpe 's{(.*)}{$1.php}'
+
+echo '\n\nTests That Do Not Cover A Class (possibly obsolete)\n\n'
 
 # This line munges the name of .*Tests.php files to .*Test.php but
 # meh, we shouldn't be doing that any more anyway.
 cat /tmp/tests_without_classes | perl -lpe 's{(.*)}{$1Test.php}'
-
-echo '\n\nClasses without tests\n\n'
-
-cat /tmp/classes_without_tests | perl -lpe 's{(.*)}{$1.php}'
