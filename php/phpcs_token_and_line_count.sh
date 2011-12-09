646848ec53a2b2d40904864114180df10d615e4d
Noah Sussman <noah@etsy.com>
2011-12-09 15:04:48 +0000

    phpcs token count: sort lines by token count

--- php/phpcs_token_and_line_count.sh
+++ php/phpcs_token_and_line_count.sh
@@ -1,3 +1,3 @@
 # Just print the line and token count per file, no code sniffer results.
 
-phpcs -v --standard=Squiz $1 | grep 'tokens' | cut -d' ' -f2-7
+phpcs -v --standard=Squiz $1 | grep 'tokens' | cut -d' ' -f2-7 | sort -rn -k5
