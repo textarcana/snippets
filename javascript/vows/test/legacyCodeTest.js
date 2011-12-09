31fd1b909d0ca83bae3b189bf77a39261b73717f
Noah Sussman <nsussman@etsy.com>
2011-12-09 00:26:50 -0500

    example of how to test browser JS with Node, JSDOM and Vows

--- javascript/vows/test/legacyCodeTest.js
+++ javascript/vows/test/legacyCodeTest.js
@@ -1,7 +1,19 @@
-document = require("jsdom").jsdom("<html><head></head><body></body></html>");
-window = document.createWindow();
-navigator = window.navigator;
-location = window.location;
-CSSStyleDeclaration = window.CSSStyleDeclaration;
+var vows = require('vows'),
+   assert = require('assert');
 
-process.env.TZ = "America/New_York";
+require('./env');
+require('../legacyCodeExample');
+
+document.body.innerHTML = '<div id="foo"></div><div id="bar"></div>';
+
+vows.describe('A function that searches for classnames').addBatch({
+    'puts the values in a global array': {
+        topic: function () {
+            return getFoo();
+        },
+        'which should contain all the class names': function (topic) {
+            assert.equal (topic, 'foo');
+        }
+    }
+}).export(module);
+
