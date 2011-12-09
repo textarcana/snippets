b120f6fa2c2c6839a8c01cc830fe7dde3e5e4074
Noah Sussman <nsussman@etsy.com>
2011-12-09 00:28:50 -0500

    comment

--- javascript/vows/legacyCodeExample.js
+++ javascript/vows/legacyCodeExample.js
@@ -1,6 +1,10 @@
 /**
  * Here is some JS designed for use in a browser.  But we can test it *
  * on the command line using Vows and JSDOM.
+ *
+ * Note that I am intentionally not declaring global variables with
+ * the var keyword.  Global variables declared with var are ignored by
+ * Node.
  */
 
 getFoo = function () {
