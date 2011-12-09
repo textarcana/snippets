351503f26ec6f9ed2477d3e63881cbc138a1e20a
Noah Sussman <nsussman@etsy.com>
2011-12-09 14:56:11 +0000

    improve PHPCS wrapper scripts

--- php/phpcs_cyclomatic_complexity_check.sh
+++ php/phpcs_cyclomatic_complexity_check.sh
@@ -3,4 +3,4 @@
 #
 # See also http://stackoverflow.com/questions/4457715/pear-codesniffer-cyclomatic-complexity
 
-phpcs -v --sniffs=Generic.Metrics.CyclomaticComplexity --standard=Squiz .
+phpcs -v --sniffs=Generic.Metrics.CyclomaticComplexity --standard=Squiz $1
