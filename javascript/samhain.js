fbe3bc8fa35f81c26632d922bc7fc62be93e7262
Noah Sussman <nsussman@etsy.com>
2011-12-28 17:38:57 -0500

    print the Samhain skull in the Firebug or Chrome console ~.~

---
+++ javascript/samhain.js
@@ -0,0 +1,26 @@
+/**
+ * Print the Samhain skull in the console.
+ * See http://www.ascii-art.de/ascii/s/skull.txt
+ */
+
+console.log('          .                                                      .');
+console.log('        .n                   .                 .                  n.');
+console.log('  .   .dP                  dP                   9b                 9b.    .');
+console.log(' 4    qXb         .       dX                     Xb       .        dXp     t');
+console.log('dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb');
+console.log('9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP');
+console.log(' 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP');
+console.log('  `9XXXXXXXXXXXXXXXXXXXXX\'~   ~`OOO8b   d8OOO\'~   ~`XXXXXXXXXXXXXXXXXXXXXP\'');
+console.log('    `9XXXXXXXXXXXP\' `9XX\'          `98v8P\'          `XXP\' `9XXXXXXXXXXXP\'');
+console.log('        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~');
+console.log('                       )b.  .dbo.dP\'`v\'`9b.odb.  .dX(');
+console.log('                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.');
+console.log('                     dXXXXXXXXXXXP\'   .   `9XXXXXXXXXXXb');
+console.log('                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb');
+console.log('                    9XXb\'   `XXXXXb.dX|Xb.dXXXXX\'   `dXXP');
+console.log('                    `\'      9XXXXXX(   )XXXXXXP      `\'');
+console.log('                              XXXX X.`v\'.X XXXX');
+console.log('                              XP^X\'`b   d\'`X^XX');
+console.log('                              X. 9  `   \'  P )X');
+console.log('                              `b  `       \'  d\'');
+console.log('                               `             \'');
