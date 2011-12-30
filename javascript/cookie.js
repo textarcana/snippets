aaab10d2ec1b22e61bce3be313e41ea6683e5bf8
Noah Sussman <nsussman@etsy.com>
2011-12-29 21:54:18 -0500

    whitespace

--- javascript/cookie.js
+++ javascript/cookie.js
@@ -1,45 +1,45 @@
 var cookie = {
-        set : function ( id, value, expiry) {
-            if(!expiry) {
-                expiry = 365;
-            }
-
-            document.cookie = id
-                + '='
-                + value
-                +';path=/;expires='
-                + this.cookieTime(expiry);
-        },
-
-        get : function( id, defaultValue ) {
-            var re = new RegExp(id+'=(.*)');
-            var value = re.exec(document.cookie);
-
-            return (value) ? value[1].split(';')[0] : defaultValue;
-        },
-
-        expire : function( id ) {
-            document.cookie = id
-                + '=;' 
-                + 'path=/;expires='
-                + this.cookieTime(-1);
-
-            this.set(id, this.get(id, false), -1);
-        },
-
-        cookieTime : function (days){
-            if (days == -1) {
-                return -1;
-            }
-
-            var now = new Date();
-            var exp = new Date();
-            var x   = Date.parse(now) + days*24*60*60*1000;
-            var re = '/(\d\d)\s(\w\w\w)\s\d\d(\d\d))/';
-
-            exp.setTime(x);
-            str = exp.toUTCString();
-
-            return str.replace(re,"$1-$2-$3");
+    set : function ( id, value, expiry) {
+        if(!expiry) {
+            expiry = 365;
         }
+
+        document.cookie = id
+            + '='
+            + value
+            +';path=/;expires='
+            + this.cookieTime(expiry);
+    },
+
+    get : function( id, defaultValue ) {
+        var re = new RegExp(id+'=(.*)');
+        var value = re.exec(document.cookie);
+
+        return (value) ? value[1].split(';')[0] : defaultValue;
+    },
+
+    expire : function( id ) {
+        document.cookie = id
+            + '=;' 
+            + 'path=/;expires='
+            + this.cookieTime(-1);
+
+        this.set(id, this.get(id, false), -1);
+    },
+
+    cookieTime : function (days){
+        if (days == -1) {
+            return -1;
+        }
+
+        var now = new Date();
+        var exp = new Date();
+        var x   = Date.parse(now) + days*24*60*60*1000;
+        var re = '/(\d\d)\s(\w\w\w)\s\d\d(\d\d))/';
+
+        exp.setTime(x);
+        str = exp.toUTCString();
+
+        return str.replace(re,"$1-$2-$3");
     }
+}
