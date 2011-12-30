712ba83151401f116f35c3544004a0ff78e65277
Noah Sussman <nsussman@etsy.com>
2011-12-29 19:45:06 -0500

    use script.onload to avoid calling jQuery before that object has been loaded
    see http://unixpapa.com/js/dyna.html

--- javascript/jquery_bootstrap.js
+++ javascript/jquery_bootstrap.js
@@ -1,45 +1,35 @@
 /**
- * Load jQuery in compatibility mode.  Intended for use in hostile
- * environments :) where another JS library has previously been loaded.
- * 
+ * Dynamically load jQuery in compatibility mode.  Intended for use in
+ * hostile environments :) where another JS library has previously
+ * been loaded.
+ *
  * See http://stackoverflow.com/questions/2074982
+ * Also see http://unixpapa.com/js/dyna.html
  */
 
 (function () {
 
   var remoteScript = document.createElement('script');
+  var compatible = document.createTextNode('jQuery.noConflict();');
+  var compatibilityScript = document.createElement('script');
+
   remoteScript.type = "text/javascript";
-  remoteScript.src =
-    "https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js";
+  remoteScript.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js";
+
+  remoteScript.onload = function(){
+
+    compatibilityScript.type = "text/javascript";
+    compatibilityScript.appendChild(compatible);
+
+    document
+      .getElementsByTagName('head')[0]
+      .appendChild(compatibilityScript);
+
+  };
+
   document
     .getElementsByTagName('head')[0]
     .appendChild(remoteScript);
-
-
-    var jqueryBootstrap = function () {
-
-        var compatible = document.createTextNode('jQuery.noConflict();');
-        var compatibilityScript = document.createElement('script');
-
-        compatibilityScript.type = "text/javascript";
-
-        compatibilityScript.appendChild(compatible);
-
-        document
-            .getElementsByTagName('body')[0]
-            .appendChild(compatibilityScript);
-
-    };
-
-    window.addEventListener('load',
-                            function(){
-                                jqueryBootstrap();
-
-                              /* now you can call your scripts that
-                              reference jQuery */
-
-                            },
-                            false);                         
 
 }());
 
