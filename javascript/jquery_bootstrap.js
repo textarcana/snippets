280fcf15d8691220793f0b404b8fea4f05bcf1be
Noah Sussman <nsussman@etsy.com>
2011-12-29 21:49:09 -0500

    dynamic jQuery noConflict bootstrap for use where Prototype is already loaded
    loads jQuery and then executes a callback function as soon as jQuery
    is finished loading

--- javascript/jquery_bootstrap.js
+++ javascript/jquery_bootstrap.js
@@ -1,22 +1,24 @@
 /**
- * Dynamically load jQuery in compatibility mode.  Intended for use in
- * hostile environments :) where another JS library has previously
- * been loaded.
+ * Dynamically load jQuery in compatibility mode and then execute a
+ * callback function once jQuery is loaded.
+ *
+ * Intended for use in hostile environments :) where another JS
+ * library has previously been loaded.
  *
  * See http://stackoverflow.com/questions/2074982
  * Also see http://unixpapa.com/js/dyna.html
  */
 
-(function () {
+var jQueryBootstrap = function (callback) {
 
   var remoteScript = document.createElement('script');
-  var compatible = document.createTextNode('jQuery.noConflict();');
-  var compatibilityScript = document.createElement('script');
 
   remoteScript.type = "text/javascript";
   remoteScript.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js";
 
   remoteScript.onload = function(){
+    var compatible = document.createTextNode('jQuery.noConflict();');
+    var compatibilityScript = document.createElement('script');
 
     compatibilityScript.type = "text/javascript";
     compatibilityScript.appendChild(compatible);
@@ -25,11 +27,11 @@
       .getElementsByTagName('head')[0]
       .appendChild(compatibilityScript);
 
+    callback();
+
   };
 
   document
     .getElementsByTagName('head')[0]
     .appendChild(remoteScript);
-
-}());
-
+};
