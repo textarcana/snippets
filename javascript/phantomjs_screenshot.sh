f50ff1bdeec2808e1a1716541b597ba6b1e60cdf
Noah Sussman <ns@noahsussman.com>
2012-06-26 20:04:21 -0400

    print a more sensible message if a URL parameter isn't provided and print the name of the outfile to STDOUT, like rasterize.js does

--- javascript/phantomjs_screenshot.sh
+++ javascript/phantomjs_screenshot.sh
@@ -1,7 +1,11 @@
+# Produce time-stamped screenshots of remote Web pages.
+
 export FILE_NAME=`echo $1 | tr -s ':/' '-' | perl -pe 's/http-//'`
 export DATE_STRING=`date +%m-%d-%Y-%H-%M-%S-%Z`.png
 export OUTFILE=$FILE_NAME"_"$DATE_STRING
 
-/usr/bin/phantomjs rasterize.js $1 $OUTFILE
+test -z $1 && echo 'Usage: ./phantomjs_screenshot.sh <url>'
+test -z $1 && exit 1
 
-
+/usr/bin/phantomjs phantomjs_rasterize.js $1 $OUTFILE && \
+    echo $OUTFILE
