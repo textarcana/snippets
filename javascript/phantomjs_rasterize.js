a3f18929188810514b0f9e8644bca5a336d3a698
Noah Sussman <ns@noahsussman.com>
2012-06-26 22:07:50 -0400

    larger viewport, faster polling

--- javascript/phantomjs_rasterize.js
+++ javascript/phantomjs_rasterize.js
@@ -13,7 +13,12 @@ if (phantom.args.length < 2 || phantom.args.length > 3) {
 } else {
     address = phantom.args[0];
     output = phantom.args[1];
-    page.viewportSize = { width: 600, height: 600 };
+
+    page.viewportSize = { 
+      width: 1920, 
+      height: 1080 
+    };
+
     page.open(address, function (status) {
         if (status !== 'success') {
             console.log('Unable to load the address!');
@@ -21,7 +26,7 @@ if (phantom.args.length < 2 || phantom.args.length > 3) {
             window.setTimeout(function () {
                 page.render(output);
                 phantom.exit();
-            }, 200);
+            }, 100);
         }
     });
 }
