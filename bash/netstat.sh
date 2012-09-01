40b4b61a837a3e0c75f01ac5551f0fce9ef199ad
Noah Sussman <ns@noahsussman.com>
2012-09-01 10:47:50 -0400

    use lsof to show a sorted list of listening ports and services (on OSX)

--- bash/netstat.sh
+++ bash/netstat.sh
@@ -2,3 +2,9 @@
 # belong to. See http://serverfault.com/questions/11289/2699
 
 sudo lsof -i -n -P | grep LISTEN
+
+# Show a sorted list of listening ports and services.
+
+sudo lsof -i -n -P | \
+    perl -lnwe 'm{^(\w+).*:(\d+) \(LISTEN} and print qq{$2\t$1}' | \
+    sort -n | uniq
