c07816d9a884900aa4f1ff7219cde0cbf617448d
Noah Sussman <ns@noahsussman.com>
2012-09-01 11:14:42 -0400

    run lsof in a while loop to get a console that shows listening ports

--- bash/netstat.sh
+++ bash/netstat.sh
@@ -8,3 +8,14 @@ sudo lsof -i -n -P | grep LISTEN
 sudo lsof -i -n -P | \
     perl -lnwe 'm{^(\w+).*:(\d+) \(LISTEN} and print qq{$2\t$1}' | \
     sort -n | uniq
+
+# Refresh the list automatically
+
+while true;do \
+    sudo lsof -i -n -P | \
+    perl -lnwe 'm{^(\w+).*:(\d+) \(LISTEN} and print qq{$2\t$1}' | \
+    sort -n | \
+    uniq && \
+    sleep 5 && \
+    clear; \
+    done
