0c7acdca9bb196520b8db823ee562866ccbb94e8
Noah Sussman <ns@noahsussman.com>
2012-10-15 18:27:00 -0400

    commits by author within date range

--- bash/git_tricks.sh
+++ bash/git_tricks.sh
@@ -5,3 +5,7 @@ git clean -dfx
 
 git log --format="%an" --since="yesterday" . | sort | uniq
 git log --format="%an" --since="June 20 2010" . | sort | uniq
+
+# commits by author within date range
+
+git log --since='1 days ago' --date-order --format='%ad %ae' | awk '{print $2,$3,$5,$7}' | uniq -c | tac | sparkler
