a1434b966bb64e0b360a255851860a5abaaea67a
Noah Sussman <ns@noahsussman.com>
2012-10-22 03:58:32 -0400

    use position:fixed to avoid centering problems when the document body has been repositioned

--- javascript/center.js
+++ javascript/center.js
@@ -6,13 +6,30 @@
  *
  * using all global variables for ease of debugging, not recommended
  * for production
+ *
+ * one of the keys here is the use of position:fixed, which causes the
+ * centered element to be positioned relative to the browser window
+ * rather than the document body. This means you can reposition the
+ * body and it won't affect the position of the centered element.
  */
 
-var target, reposition, center, center2, stayCentered;
+var target, reposition, center, center2, stayCentered, getXY;
 
 stayCentered = function  (el) {
     
     target = el;
+
+    getXY = function (el) {
+        var _x = 0;
+        var _y = 0;
+        while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+            _x += el.offsetLeft - el.scrollLeft;
+            _y += el.offsetTop - el.scrollTop;
+            el = el.offsetParent;
+        }
+        return [_x, _y];
+    }
+
 
     center = function (property1, property2, property3) {
         console.log('window[%s]=', property2, window[property2]);
@@ -27,7 +44,7 @@ stayCentered = function  (el) {
         center('top', 'innerHeight', 'clientHeight');
     };
 
-    el.style.position='absolute';
+    el.style.position='fixed';
     el.style.top='0';
 
     window.onresize=reposition;
