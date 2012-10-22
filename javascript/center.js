4d59e0d0066837c15fcf0669af6815806ad54fcd
Noah Sussman <ns@noahsussman.com>
2012-10-22 03:11:50 -0400

    I can still break the centering by moving the document body off screen

--- javascript/center.js
+++ javascript/center.js
@@ -1,47 +1,28 @@
 /* See http://stackoverflow.com/a/1248126/55478 
  * and http://stackoverflow.com/a/442474/55478
+ *
+ * needs some work for compatibility with older MSIE versions -- with
+ * regard to clientWidth/Height and innerWidth/Height
+ *
+ * using all global variables for ease of debugging, not recommended
+ * for production
  */
 
-var stayCentered = function  (el) {
+var elem, getOffset, reposition, center, center2, stayCentered;
 
-    var elem, getOffset, reposition, center, center2;
+stayCentered = function  (el) {
 
-    elem = (document.compatMode === "CSS1Compat") ? 
-        document.documentElement :
-        document.body;
-
-    getOffset = function (el) {
-        var _x = 0;
-        var _y = 0;
-        while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-            _x += el.offsetLeft - el.scrollLeft;
-            _y += el.offsetTop - el.scrollTop;
-            el = el.offsetParent;
-        }
-        return { top: _y, left: _x };
-    }
-
-    center = function (property1, property2) {
-        document.
-            getElementById('shout').
-            style[property1] = elem[property2]/2-getOffset(el)[property1]/2+'px';
-    }
-
-    center2 = function (property1, property2) {
-
+    center = function (property1, property2, property3) {
         console.log('window[%s]=', property2, window[property2]);
+        console.log('el[%s]=', property3, window[property2]);
         console.log('CSS %s=', property1, el.style[property1]);
-        document.
-            getElementById('shout').
-            style[property1] = window[property2]/2+'px';
+        el.style[property1] = window[property2]/2 - el[property3] + 'px';
     }
 
     reposition = function(){
         el.style.margin = 0;
-        el.style.marginLeft = '-150px';
-        el.style.marginTop = '-150px';
-        center2('left', 'innerWidth');
-        center2('top', 'innerHeight');
+        center('left', 'innerWidth', 'clientWidth');
+        center('top', 'innerHeight', 'clientHeight');
     };
 
     el.style.position='absolute';
@@ -52,5 +33,3 @@ var stayCentered = function  (el) {
     reposition();
 
 }
-
-stayCentered(document.getElementById('shout'));
