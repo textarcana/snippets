7844877b55abb45f33910d6180fb933743e813fb
Noah Sussman <ns@noahsussman.com>
2012-10-28 01:15:29 -0400

    using points for task estimation ftw

--- meta/estimate/estimate_for_job.rb
+++ meta/estimate/estimate_for_job.rb
@@ -1,16 +1,18 @@
 def estimate
 
-puts 'how long will it take?'
-t1 = gets
+  puts 'best case, how many points of effort?'
+  t1 = gets
 
-puts 'no, *honestly,* how long will it take?'
-t2 = gets
+  puts 'given current conditions, how many points?'
+  t2 = gets
 
-puts 'in the absolute WORST case, how long would it take?'
-t3 = gets
+  puts 'in the absolute WORST case, how many points would it take?'
+  t3 = gets
 
 
-puts "\nOk then I estimate it will really take this long:"
-puts ((t1.to_i + (4 * t2.to_i) + t3.to_i) / 6).to_s
+  puts "\nOk then I estimate it will really take this long:"
+  puts ((t1.to_i + (4 * t2.to_i) + t3.to_i) / 6).to_s
 
 end
+
+estimate
