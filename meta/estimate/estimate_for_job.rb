27be496492b34f631d6522f7dfbec07f8566c039
Noah Sussman <ns@noahsussman.com>
2012-10-28 01:57:28 -0400

    use points not hours for estimation

--- meta/estimate/estimate_for_job.rb
+++ meta/estimate/estimate_for_job.rb
@@ -10,7 +10,7 @@ def estimate
   t3 = gets
 
 
-  puts "\nOk then I estimate it will really take this long:"
+  puts "\nOk then I estimate that task at this many points:"
   puts ((t1.to_i + (4 * t2.to_i) + t3.to_i) / 6).to_s
 
 end
