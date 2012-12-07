12aa21db7a75dfdb2f8d8eec53961b6e1135c1de
Noah Sussman <ns@noahsussman.com>
2012-12-07 18:27:35 -0500

    movies with gource

--- code_quality/gource.sh
+++ code_quality/gource.sh
@@ -3,3 +3,8 @@
 # the code in backticks provides the name of the active git branch
 
 gource --title `git symbolic-ref HEAD | cut -d'/' -f3` --key --highlight-users --hide filenames
+
+# Generate a movie.
+# See http://rubyist-journal.com/2011/05/05/create-artwork-with-gource/
+
+gource  --title `git symbolic-ref HEAD | cut -d'/' -f3` --key --font-size 20 --highlight-all-users --camera-mode track --stop-at-end -s 0.2 -i 63072000 --hide filenames,bloom,progress,mouse -o - | ffmpeg -y -b 3000K -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -vpre placebo -threads 0 video.mp4
