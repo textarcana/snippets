a29dee81bd7e13c7373685f719b5aae767b039fd
Noah Sussman <ns@noahsussman.com>
2012-12-07 18:33:59 -0500

    I like the bloom filter

--- code_quality/gource.sh
+++ code_quality/gource.sh
@@ -7,7 +7,7 @@ gource --title `git symbolic-ref HEAD | cut -d'/' -f3` --key --highlight-users -
 # Generate a movie.
 # See http://rubyist-journal.com/2011/05/05/create-artwork-with-gource/
 
-gource  --title `git symbolic-ref HEAD | cut -d'/' -f3` --key --font-size 20 --highlight-all-users -1920x1080 --stop-at-end -s 0.2 -i 63072000 --hide filenames,bloom,progress,mouse -o - | ffmpeg -y -b 3000K -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -threads 0 video.mp4
+gource  --title `git symbolic-ref HEAD | cut -d'/' -f3` --key --font-size 20 --highlight-all-users -1920x1080 --stop-at-end -s 0.2 -i 63072000 --hide filenames,progress,mouse -o - | ffmpeg -y -b 3000K -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -threads 0 video.mp4
 
 
 
